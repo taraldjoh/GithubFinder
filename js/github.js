06aea007930e94b65ce2a4b4e1fc081c1bf9f976
Tarald Johansen <41441415+taraldjoh@users.noreply.github.com>
2019-04-10 22:11:52 +0200

    get user function

--- js/github.js
+++ js/github.js
@@ -3,4 +3,14 @@ class GitHub {
         this.client_id = 'ee39a193c5efde9ff400';
         this.client_secret = 'a353f1c5214dc40f2d99369ba4283b306dd69022';
     }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+
+        return {
+           profile
+        }
+    }
 }

e38b6be688c521f9a699c01bab96fd34fe56d4d8
Tarald Johansen <41441415+taraldjoh@users.noreply.github.com>
2019-04-16 18:07:09 +0200

    fetch data from github

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
+// Init Github
+const github = new Github();
+
 // Search input
 const searchUser = document.getElementById("searchUser");
 
 // Search input event listener
-searchUser.addEventListener('keyup',(e) => {
-    // Get input text
-    const userText = e.target.value;
-    
-    if(userText !== ''){
-        console.log(userText);
-    }
+searchUser.addEventListener("keyup", e => {
+  // Get input text
+  const userText = e.target.value;
+
+  if (userText !== "") {
+    // Make HTTP Call
+    github.getUser(userText).then(data => {
+      console.log(data);
+    });
+  }
 });
--- js/github.js
+++ js/github.js
@@ -1,16 +1,20 @@
-class GitHub {
-    constructor() {
-        this.client_id = 'ee39a193c5efde9ff400';
-        this.client_secret = 'a353f1c5214dc40f2d99369ba4283b306dd69022';
-    }
+class Github {
+  constructor() {
+    this.client_id = "ee39a193c5efde9ff400";
+    this.client_secret = "a353f1c5214dc40f2d99369ba4283b306dd69022";
+  }
 
-    async getUser(user) {
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
 
-        const profile = await profileResponse.json();
+    const profile = await profileResponse.json();
 
-        return {
-           profile
-        }
-    }
+    return {
+      profile
+    };
+  }
 }
